2dc3aea53acc2b78939f1739ba55469b4807158e
Gabriel Gomes <ggvgabriel05@gmail.com>
2023-02-15 12:38:23 -0300

    First UP

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import { Input } from './components/input'
+import { Stopwatch } from './components/stopwatch'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+
+    </div>
+  )
+}
+
+export default App
